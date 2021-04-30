0e007685b6473dc7ed26006c622ca2856e022bb4
KaustavS24 <81281308+KaustavS24@users.noreply.github.com>
2021-04-30 16:43:55 +0530

    Update Dao.java

--- CRUD_/src/main/java/dao/Dao.java
+++ CRUD_/src/main/java/dao/Dao.java
@@ -16,12 +16,10 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import com.dao.rowmapper.EmployeeRowMapper;
 
 import model.Employee;
-//import connectionUtil.ConnectionUtil;
+
 
 public class Dao implements EmployeeDaoInt {
 	
-	//ConnectionUtil util = new ConnectionUtil();
-	//Connection con = util.getConnection();
 	
 	JdbcTemplate jdbcTemplate;
 	
@@ -48,30 +46,7 @@ public class Dao implements EmployeeDaoInt {
 		namedParameterJdbcTemplate.update(INSERT_SQL, parameters);
 	}
 
-	//public long addEmployee(Employee emp)
-	//{
-		//try
-		//{
-			//String query = "insert into Employee values(?,?,?,?)";
-			//PreparedStatement st = con.prepareStatement(query);
-			//st.setLong(1, emp.getEmpId());
-			//st.setString(2, emp.getEmpName());
-			//st.setInt(3, emp.getEmpAge());
-			//st.setString(4, emp.getEmpAddress());
-			
-			//int count = st.executeUpdate();
-			//if (count != 0)
-			//{
-				//return emp.getEmpId();
-			//}
-		//}
-		//catch(Exception ex)
-		//{
-			//System.out.println("Cannont insert details.");
-	//	}
-		//return 0;
-		
-	//}
+	
 	
 	public List<Employee> getEmployee()
 	{
@@ -80,35 +55,7 @@ public class Dao implements EmployeeDaoInt {
 		return results;
 	}
 	
-	//public HashMap<Long, Employee> getEmployee()
-	//{
-		//HashMap<Long, Employee> empMap = new HashMap<>();
-		//Employee em = new Employee();
-		
-		//try
-		//{
-			//String query = "select * from Employee";
-			//PreparedStatement st = con.prepareStatement(query);
-			//ResultSet rs = st.executeQuery(query);
-			//while(rs.next())
-			//{
-				//em.setEmpId(rs.getLong(1));
-				//em.setEmpName(rs.getString(2));
-				//em.setEmpAge(rs.getInt(3));
-				//em.setEmpAddress(rs.getString(4));
-				
-			//	empMap.put(em.getEmpId(), em);
-		//	}
-			
-			//return empMap;
-		//}
-		//catch(Exception ex)
-		//{
-			//System.out.println("Error");
-		//}
-		//return null;
-		
-	//}
+	
 	
 	public Employee getEmployeebyId(long id)
 	{
@@ -123,33 +70,7 @@ public class Dao implements EmployeeDaoInt {
 		return null;
 	}
 	
-	//public Employee getEmployeebyId(long id)
-	//{
-		//Employee em = new Employee();
-		
-		//try
-		//{
-			//String query = "select * from Employee where empId=?";
-			//PreparedStatement st = con.prepareStatement(query);
-			//st.setLong(1, id);
-			
-			//ResultSet rs = st.executeQuery();
-			//while(rs.next())
-			//{
-				//em.setEmpId(rs.getLong(1));
-				//em.setEmpName(rs.getString(2));
-				//em.setEmpAge(rs.getInt(3));
-				//em.setEmpAddress(rs.getString(4));
-			//}
-		//	return em;
-	//	}
-		//catch(Exception e)
-		//{
-		//	System.out.println("Error occured");
-	//	}
-		//return null;
-		
-	//}
+
 	
 	public void deleteEmployee(long id)
 	{
@@ -160,26 +81,6 @@ public class Dao implements EmployeeDaoInt {
 	}
 	
 	
-	//public boolean deleteEmployee(long id)
-	//{
-		
-		//try {
-			//String query = "select * from Employee where empId=?";
-			//PreparedStatement st = con.prepareStatement(query);
-			//st.setLong(1, id);
-			
-			//int rs = st.executeUpdate();
-			//if(rs != 0)
-			//{
-				//return true;
-			//}
-//		} catch (SQLException e) {
-	//		System.out.println("Error occured.");
-			//e.printStackTrace();
-		//}
-		//return false;
-		
-	//}
 	
 	public void updateEmployeeName(String name, long id)
 	{
@@ -192,27 +93,6 @@ public class Dao implements EmployeeDaoInt {
 	
 	
 	
-	//public boolean updateEmployeeName(String name, long id)
-	//{
-		//try 
-		//{
-			//String query = "update Employee set empName=? where empId=?";
-			//PreparedStatement st = con.prepareStatement(query);
-			//st.setString(1, name);
-			//st.setLong(2, id);
-			//int rs = st.executeUpdate();
-			//if(rs!=0)
-			//{
-				//return true;
-			//}
-		//}
-		//catch (Exception e) 
-	//	{
-		//	System.out.println("Error Occurred.");
-		//}
-		//return false;
-	//}
-	
 	public void updateEmployeeAge(int age, long id)
 	{
 		final String UPDATE_EMPLOYEE_AGE_SQL = "update Employee set empAge = :empAge where empId = :empId";
@@ -221,26 +101,7 @@ public class Dao implements EmployeeDaoInt {
 		parameters.put("empId", id);
 		namedParameterJdbcTemplate.update(UPDATE_EMPLOYEE_AGE_SQL, parameters);
 	}
-	//public boolean updateEmployeeAge(int age, long id)
-	//{
-		//try 
-		//{
-			//String query = "update Employee set empAge=? where empId=?";
-			//PreparedStatement st = con.prepareStatement(query);
-			//st.setInt(1, age);
-			//st.setLong(2, id);
-			//int rs = st.executeUpdate();
-			//if(rs!=0)
-			//{
-				//return true;
-			//}
-		//}
-		//catch (Exception e) 
-		//{
-			//System.out.println("Error Occurred.");
-		//}
-		//return false;
-	//}
+	
 	
 	public void updateEmployeeAddress(String address, long id)
 	{
@@ -251,46 +112,6 @@ public class Dao implements EmployeeDaoInt {
 		namedParameterJdbcTemplate.update(UPDATE_EMPLOYEE_ADDRESS_SQL, parameters);
  	}
 	
-	//public boolean updateEmployeeAddress(String address, long id)
-	//{
-	//	try 
-		//{
-			//String query = "update Employee set empAddress=? where empId=?";
-			//PreparedStatement st = con.prepareStatement(query);
-			//st.setString(1, address);
-			//st.setLong(2, id);
-			//int rs = st.executeUpdate();
-			//if(rs!=0)
-		//	{
-			//	return true;
-			//}
-	//	}
-		//catch (Exception e) 
-		//{
-			//System.out.println("Error Occurred.");
-		//}
-		//return false;
-	//}
 	
-	//public long getLastEmployeeId()
-	//{
-		//long id = 0;
-		//try 
-		//{
-			//String query = "select empId from Employee where rownum <=1 order by empId desc";
-			//PreparedStatement st = con.prepareStatement(query);
-			//ResultSet rs = st.executeQuery();
-			//while(rs.next())
-			//{
-				//id = rs.getLong(1);
-			//}
-		//	return id;
-		//}
-		//catch (SQLException e)
-	//	{
-		//	System.out.println("Error Occurred.");
-		//}
-		//return 0;
-	//}
 
 }
